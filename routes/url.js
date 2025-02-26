2ab84937acb5a8ba742d2e5481af367a0813b6d8
Rushabh-K <rushabh.kalme24@vit.edu>
2025-02-26 18:56:09 +0530

    organized

--- routes/url.js
+++ routes/url.js
@@ -10,6 +10,22 @@ router.get("/", (req, res) => {
   return res.render("website/dashboard");
 });
 
+router.get("/billing", (req, res) => {
+  return res.render("website/billing");
+});
+router.get("/myTask", (req, res) => {
+  return res.render("website/myTasks");
+});
+router.get("/pomodoro", (req, res) => {
+  return res.render("website/pomodoro");
+});
+router.get("/project", (req, res) => {
+  return res.render("website/project");
+});
+router.get("/messages", (req, res) => {
+  return res.render("website/Messages");
+});
+
 router.post("/123", handleGenerateNewShortURL);
 
 router.get("/analytics/:shortId", handleGetAnalytics);
